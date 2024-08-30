680a34d6b1049037626f643960aecd7b90e780bb
Razvan-Ioan Dinita <razvan.dinita@oneregistry.co.uk>
2024-08-30 18:07:50 +0100

    updates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+    includeHTML();
+
+    function includeHTML()
+    {
+        const includes = document.querySelectorAll('[id$="-container"]');
+        includes.forEach(include => {
+            const file = include.id.split('-')[0] + '.html';
+            fetch(file)
+                .then(response => response.text())
+                .then(data => include.innerHTML = data)
+                .then(() => {
+                    if(include.id === 'footer-container') {
+                        const year = new Date().getFullYear();
+                        include.innerHTML = include.innerHTML.replace(/{{year}}/g, year.toString());
+                    }
+                });
+        });
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
@@ -17,12 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const yearSpan = document.getElementById('year');
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear.toString();
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.nav-link');
     const navCollapse = document.querySelector('.navbar-collapse');
 
@@ -35,27 +51,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-document.addEventListener('load', function() {
-    toggleNavbarVisibility();
-    window.addEventListener('resize', toggleNavbarVisibility);
-});
-
-function toggleNavbarVisibility()
-{
-    const hiddenCss = 'display: none !important';
-
-    const navbar = document.querySelector('.navbar-collapse');
-    const navbarToggler = document.querySelector('.navbar-toggler');
-
-    const contentHeight = document.body.scrollHeight;
-    const viewportHeight = window.innerHeight;
-
-    if(contentHeight <= viewportHeight) {
-        navbar.style.cssText = hiddenCss;
-        navbarToggler.style.cssText = hiddenCss;
-    } else {
-        navbar.style.display = '';
-        navbarToggler.style.display = '';
-    }
-}
